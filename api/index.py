fc9fedffada243e46f64d3846d93431a0331d95d
denggao <phpinside@qq.com>
2025-05-15 15:16:11 +0800

    update

--- api/index.py
+++ api/index.py
@@ -8,7 +8,13 @@ async def hello():
     return {'message': 'Hello from FastAPI on Vercel!'}
 
 
-@app.get('/test')
+@app.get('/test1')
 async def test():
     now = datetime.now()
     return "现在时间"+ now.strftime("%Y-%m-%d %H:%M:%S")
+
+@app.get('/test2')
+async def test2():
+    return "test2"
+
+
